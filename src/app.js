5a03fdaccfe7a53029f3f34079968a00a469e9e8
Chris Loach <chenryloach@gmail.com>
2024-07-15 15:13:19 +0100

    added 4-get-article-article-id endpoint with error handling, updated api json with this endpoint, changed the wildcard error from 404 to 400

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
-const { getTopics, getEndpoints } = require("./controllers.js")
+const { getTopics, getEndpoints, getArticleById } = require("./controllers.js")
 
 const express = require("express")
 const app = express()
+
+// Endpoints 
 
 app.get("/api", getEndpoints)
 
 app.get("/api/topics", getTopics)
 
+app.get("/api/articles/:article_id", getArticleById)
+
 app.get("/api/*", (req, res) => {
-    res.status(404).send({ message: "Bad endpoint"})
+    res.status(400).send({ message: "Bad endpoint" })
+})
+
+// Error handling 
+
+app.use((err, req, res, next) => {
+    const { status, message, code } = err
+    if (status && message) {
+        res.status(status).send({ message })
+    }
+    if (code === "22P02") {
+        res.status(400).send({ message: "Bad endpoint" })
+    }
 })
 
 module.exports = app
--- src/controllers.js
+++ src/controllers.js
@@ -1,8 +1,8 @@
-const { selectTopics } = require("./models.js")
+const { selectTopics, selectArticleById } = require("./models.js")
 const endpoints = require("../endpoints.json")
+const { articleData } = require("../db/data/test-data/index.js")
 
 exports.getEndpoints = (req, res) => {
-    console.log(typeof endpoints)
     res.status(200).send({ endpoints })
 }
 
@@ -10,5 +10,19 @@ exports.getTopics = (req, res) => {
     selectTopics()
         .then(topics => {
             res.status(200).send({ topics })
+        })
+        .catch(err => {
+            console.log("err:", err)
+        })
+}
+
+exports.getArticleById = (req, res, next) => {
+    const { article_id } = req.params
+    selectArticleById(article_id)
+        .then(article => {
+            res.status(200).send({ article })
+        })
+        .catch(err => {
+            next(err)
         })
 }
